437c2dcb0a8f7f329e862ec0f333e3b8cab35f07
samcolson4 <samcolson4@gmail.com>
2021-02-15 08:17:17 +0000

    Add home link to 404 page

--- src/components/pages/NotFoundRoute.js
+++ src/components/pages/NotFoundRoute.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 const NotFoundRoute = () => {
   return (
     <div>
-      <p>Page not found</p>
+      <p>Page not found.</p>
+      <Link to="/" className="navbar_link">
+        GO HOME      
+      </Link>
     </div>
   );
 }
