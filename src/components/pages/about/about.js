791783452d28ccec6f38fd1d179a4bea931d4605
samcolson4 <samcolson4@gmail.com>
2021-02-15 08:43:42 +0000

    Add colsondesign link

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -1,7 +1,10 @@
+import { render } from "react-dom"
+
 const About = () => {
   return (
     <div>      
       <h2>This is a project inspired by the layout of some poems in 'Just Us' by Claudia Rankine.</h2>
+      <p>This website is built by <a href="http://colsondesign.com">Sam Colson</a></p>
     </div>
   );
 }
