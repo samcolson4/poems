0207a34d692e21c47b483e73ed63d40c815f3a37
samcolson4 <samcolson4@gmail.com>
2021-02-15 09:59:58 +0000

    Add about CSS

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -1,8 +1,10 @@
+import './about.css'
+
 const About = () => {
   return (
     <div>      
       <h2>This is a project inspired by the layout of some poems in 'Just Us' by Claudia Rankine.</h2>
-      <p>This website is built by <a href="http://colsondesign.com">Sam Colson</a></p>
+      <p>This website is built by <a className="inline_link" href="http://colsondesign.com">Sam Colson</a></p>
     </div>
   );
 }
