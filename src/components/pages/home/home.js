8e2632179cc4415a8901912b35c1fee459be7149
samcolson4 <samcolson4@gmail.com>
2021-02-16 18:21:19 +0000

    Reset code to allow JSON version

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,4 +1,4 @@
-import WordsLogic from './words-logic.js'
+import WordsLogic from './wordsLogic.js'
 import React, { Component } from 'react'
 
 var words = new WordsLogic()
@@ -6,36 +6,19 @@ var words = new WordsLogic()
 class Home extends Component {
 
   state = {
-    showWords: false
-  }
-
-  updateWords = (data) => {
-    this.setState({
-      showWords: true
-    });
-  }
-
-  generatePoem = async () => {
-    var num = 40
-    let data = await words.getLotsOfWords(num)
-    while (words.outputWords.length < num){
-      console.log(words.outputWords)
-      return true
-    }
-    this.updateWords(data)
+    showWords: true
   }
 
   render() {
 
     if (this.state.showWords) {
       var wordList = (
-        <div>{ words.outputWords }</div>
+        <div>{ words.words }</div>
       )
     }
 
     return (
       <div>
-        <button onClick={ this.generatePoem }> Get words </button>
         { wordList }
       </div>
     );
---
+++ src/components/pages/home/wordsLogic.js
@@ -0,0 +1,12 @@
+class WordsLogic {
+  
+  constructor() {
+    this.words = ["This is a test"]
+    this.outputWords = []
+  }
+  
+
+ 
+}
+
+export default WordsLogic;
