5aeb25d87d53a2761b4dd73f1b87d1e58d35a7c8
samcolson4 <samcolson4@gmail.com>
2021-02-17 10:51:35 +0000

    Add personal links to about

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -3,8 +3,13 @@ import './about.css'
 const About = () => {
   return (
     <div>      
-      <h2>This is a project inspired by the layout of some poems in 'Just Us' by Claudia Rankine.</h2>
+      <p>This is a project inspired by the layout of some poems in 'Just Us' by Claudia Rankine.</p>
       <p>This website is built by <a className="inline_link" href="http://colsondesign.com">Sam Colson</a></p>
+
+      <br></br><br></br>
+      <div id="links">
+        <a className="inline_link" href="https://github.com/samcolson4/react-catan">GitHub repo</a> | <a className="inline_link" href="http://www.colsondesign.com">Project Portfolio</a> | <a className="inline_link" href="https://www.linkedin.com/samcolson">LinkedIn</a>
+      </div>
     </div>
   );
 }
